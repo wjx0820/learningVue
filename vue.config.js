d2141ccb6d21be8efe0606133d80a83278ec0d99
Jexxie <jexxie@Jexxies-iMac.local>
2018-09-08 11:53:27 +0800

    修改vue.config文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin' : '/'
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时不生产.map文件
+  productionSourceMap: false，
+  // 跨域配置 将未知请求代理到指定url
+  devServer: {
+    proxy: 'http://localhost:4000'
+  }
 }
