89ea6de88d1933d7250f87c7f7af3f03463aceb5
Jexxie <jexxie@Jexxies-iMac.local>
2018-09-09 11:40:02 +0800

    路由切换动效

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,51 @@
       <router-link :to="{name: 'home'}">Home</router-link> |
       <router-link :to="{name: 'about'}">About</router-link>
     </div>
-    <router-view/>
-    <router-view name="email" />
-    <router-view name="tel" />
+    <transition-group :name="routerTransition">
+      <router-view key="default"/>
+      <router-view key="email" name="email" />
+      <router-view key="tel" name="tel" />
+    </transition-group>
   </div>
 </template>
 
+<script>
+export default {
+  // 为页面设置特定动效
+  data () {
+    return {
+      routerTransition: ''
+    }
+  },
+  watch: {
+    '$route' (to) {
+            to.query && to.query.transitionName && (this.routerTransition = to.query.transitionName)
+    }
+  }
+}
+</script>
+
+
 <style lang="less">
+.router-enter {
+  opacity: 0;
+}
+.router-enter-active {
+  transition: opacity 1s ease;
+}
+.router-enter-to {
+  opacity: 1;
+}
+
+.router-leave {
+  opacity: 1;
+}
+.router-leave-active {
+  transition: opacity 1s ease;
+}
+.router-leave-to {
+  opacity: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
